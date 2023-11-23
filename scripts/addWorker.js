acd8e1dc8e67aaa040dbd6e97e4920d075439699
Lyadov Maxim <lyadov.maxim2018@yandex.ru>
2023-11-23 21:02:36 +0500

    Добавлена функция добавления/удаления расписаний

---
+++ scripts/addWorker.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  checkboxes.forEach(function(checkbox) {
+      checkbox.addEventListener('change', function() {
+        if (this.checked) {
+
+          let timeTable = document.querySelector('.timeTable');
+          let newWorker = document.createElement('li');
+          newWorker.classList.add('oneWorker');
+          newWorker.innerHTML = `
+            <div class="date">20.20.2023</div>
+            <div class="FIO"></div>
+            <ul class="schedule">
+              <li></li>
+            </ul>
+          `;
+          timeTable.append(newWorker);
+
+          //заполнение списка времени записи
+          let allScheduleList = document.querySelectorAll('.schedule');
+          let lastscheduleList = allScheduleList[allScheduleList.length - 1];
+
+          lastscheduleList.firstChild.remove();
+          
+          let time = new Date();
+          time.setHours(8, 0, 0);
+          for (let i = 0; i < 21; i++) {
+            let newItem = document.createElement('li');
+            let hours = time.getHours();
+            let minuts = time.getMinutes();
+            if(minuts == "0")
+              minuts = "00";
+            newItem.innerHTML = (hours +":" + minuts);
+            lastscheduleList.append(newItem);
+            time.setMinutes(time.getMinutes() + 10)
+          }
+
+        //заполнение ФИО
+        let name = this.nextElementSibling.textContent;
+        
+        let allFIO = document.querySelectorAll('.FIO');
+        let lastFIO = allFIO[allFIO.length - 1];
+        lastFIO.innerHTML = name
+              
+          } else {
+            let name = this.nextElementSibling.textContent;
+            let timeTable = document.querySelector(".timeTable");
+            let oneWorker = timeTable.querySelectorAll(".oneWorker");
+
+            for (let i = 0; i < oneWorker.length; i++) {
+              let workerFIO = oneWorker[i].querySelector(".FIO").textContent;
+
+              if (workerFIO == name) {
+                oneWorker[i].remove();
+              }
+            }
+          }
+      });
+  });
+});

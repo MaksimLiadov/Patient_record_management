f4c3862c5d5222a103514044c5d32732c757a714
Lyadov Maxim <lyadov.maxim2018@yandex.ru>
2024-03-02 19:37:13 +0500

    Исправления: Удалены лишние alert

--- scripts/charts/library/echart_each_day_of_the_week.js
+++ scripts/charts/library/echart_each_day_of_the_week.js
@@ -2,14 +2,10 @@ import { getDayOfWeek } from "../get_map/get_map.js";
 
 let DayOfWeek = getDayOfWeek();
 
-let a = Object.keys(DayOfWeek);
-alert(a.length);
-
 let numberAppointment = [];
 for (let day in DayOfWeek) {
     numberAppointment.push(DayOfWeek[day]);
 }
-alert(numberAppointment);
 
 let optionChartDayOfWeek = {
     title: {
